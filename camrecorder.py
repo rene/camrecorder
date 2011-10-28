6715428fc25ca7c64c3261c8e18a92724177e876
Renê S. Pinto <rene@renesp.com.br>
2011-10-28 14:04:01 -0200

    Sending video from camera to icecast and file.

--- camrecorder.py
+++ camrecorder.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
-import sys
 from optparse import OptionParser
+import glib, gtk
 
 try:
 	import pygst
@@ -19,7 +19,18 @@ class CamRecorder:
 	ST_STREAMING    = 1
 	ST_PAUSED       = 2
 
-
+	##
+	# Class Constructor
+	#
+	# The pipeline for this application is:
+	#                                                                   --------   ------------
+	#                                                                |-| queue0 |-| shout2send |
+	#   ---------   --------------   -----------   --------   -----  |  --------   ------------
+	#  | v4l2src |-| clockoverlay |-| theoraenc |-| oggmux |-| tee |-|
+	#   ---------   --------------   -----------   --------   -----  |  --------   ----------
+	#                                                                |-| queue1 |-| filesink |
+	#                                                                   --------   ----------
+	#
 	def __init__(self):
 
 		self.device   = '/dev/video0'
@@ -29,16 +40,123 @@ class CamRecorder:
 		self.mount    = ''
 		self.password = None
 		
-		self.status = ST_NOT_STARTED
+		self.status = CamRecorder.ST_NOT_STARTED
 
+		# Create pipeline
 		self.pipeline = gst.Pipeline('camrec-pl')
 
+		# Elements
+		self.tee    = gst.element_factory_make('tee', 'tee1')
+		self.queue0 = gst.element_factory_make('queue', 'queue0')
+		self.queue1 = gst.element_factory_make('queue', 'queue1')
 
-	def start(self, ip, port, mount, password):
+		# Video
+		self.videosrc     = gst.element_factory_make('v4l2src', 'videosrc')
+		self.clockoverlay = gst.element_factory_make('clockoverlay', 'clock')
+		self.theoraenc    = gst.element_factory_make('theoraenc', 'theora')
+		self.oggmux       = gst.element_factory_make('oggmux', 'oggm')
+
+		# Icecast streaming
+		self.shout = gst.element_factory_make('shout2send', 'shout')
+
+		# Video recorder
+		self.filesink = gst.element_factory_make('filesink', 'fout')
+		
+		# Connect elements
+		self.pipeline.add(self.videosrc, self.clockoverlay, self.theoraenc, self.oggmux, self.tee, self.queue0, self.queue1, self.filesink, self.shout)
+		gst.element_link_many(self.videosrc, self.clockoverlay, self.theoraenc, self.oggmux, self.tee)
+		gst.element_link_many(self.queue0, self.shout)
+		gst.element_link_many(self.queue1, self.filesink)
+
+		# Connect tee pads
+		teepad0 = self.tee.get_request_pad('src0')
+		teepad1 = self.tee.get_request_pad('src1')
+		teepad0.link(self.queue0.get_pad('sink'))
+		teepad1.link(self.queue1.get_pad('sink'))
+
+		# Bus
+		bus = self.pipeline.get_bus()
+		bus.add_signal_watch()
+		bus.connect("message", self.cb_messages)
+
+
+
+	##
+	# Start streaming and recording server.
+	#
+	# \param device Video device (i.e. /dev/video0).
+	# \param ip Icecast server ip.
+	# \param port Icecast server port.
+	# \param mount Source client mount name.
+	# \param password Icecast server password.
+	#
+	def start(self, device, ip, port, mount, password):
+
+		self.device   = device
+		self.ip       = ip
+		self.port     = port
+		self.mount    = mount
+		self.password = password
+
+		# Set element properties
+
+		# Video source
+		self.videosrc.set_property('device', device)
+		
+		# Clock overlay
+		self.clockoverlay.set_property('halign', 'right')
+		self.clockoverlay.set_property('valign', 'top')
+		self.clockoverlay.set_property('shaded-background', 'true')
+		self.clockoverlay.set_property('time-format', '%d/%m/%Y %H:%M:%S')
+	
+		# Theora enc
+		self.theoraenc.set_property('quality', 48)
+		
+		# Filesink
+		self.filesink.set_property('location', 'teste.ogg')
+
+		# Shoutcast
+		self.shout.set_property('ip', self.ip)
+		self.shout.set_property('port', self.port)
+		
+		if self.password != None:
+			self.shout.set_property('password', self.password)
+		if self.mount != None:
+			self.shout.set_property('mount', self.mount)
+
+		self.pipeline.set_state(gst.STATE_PLAYING)
+
+		print 'Using video device: %s' % device
 		print 'Starting server:'
 		print '    ip:    %s' % ip
 		print '    port:  %d' % port
 		print '    mount: %s' % mount
+
+
+	def cb_messages(self, bus, message):
+		
+		t = message.type
+		if t == gst.MESSAGE_STATE_CHANGED:
+
+			old, new, pending = message.parse_state_changed()
+
+			if new == gst.STATE_PLAYING:
+				print 'Playing...'
+			elif new == gst.STATE_NULL:
+				print 'Null'
+			elif new == gst.STATE_PAUSED:
+				print 'Paused'
+
+		elif t == gst.MESSAGE_ERROR:
+			err, debug = message.parse_error()
+			print "Error: %s" % err, debug
+			return
+		
+		elif t == gst.MESSAGE_BUFFERING:
+			print 'Buffering...'
+
+		return
+
 
 # main
 if __name__ == '__main__':
@@ -68,5 +186,13 @@ if __name__ == '__main__':
 		port = int(options.port)
 
 	server = CamRecorder()
-	server.start(options.ip, port, options.mount, options.password)
+	server.start(options.device, options.ip, port, options.mount, options.password)
+
+	# Enter to GLib MainLoop
+	gtk.gdk.threads_init()
+	try:
+		gtk.main()
+	except KeyboardInterrupt:
+		server.pipeline.set_state(gst.STATE_NULL)
+		print 'Good Bye!'
 
---
+++ teste.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./camrecorder.py -d /dev/video1 -a 10.1.1.2 -p 2180 -s rene -m live3.ogg
+
